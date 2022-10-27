3f2dfcbadb4afbedff433f878a4a8e1066922578
GrannyYetta <you@example.com>
2022-10-27 12:59:53 +0200

    game menu updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,33 @@
-import { Link, useMatch, useResolvedPath } from "react-router-dom"
-
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import GameMenu from "./GameMenu";
 
 function Navbar() {
-    
-return (
+  return (
     <nav className="nav">
-        <Link to="/" className="site-title">
-            Feminist Tech Card Game
-        </Link>
-        <ul>
-            <CustomLink to="/about">About</CustomLink>
-            <CustomLink to="/disclaimer">Disclaimer</CustomLink>
-            <CustomLink to="/download">Download Card Deck</CustomLink>
-            <CustomLink to="/superrr">Superrr Webiste</CustomLink>
-        </ul>
+      <Link to="/" className="site-title">
+        Online Feminist Tech Card Deck
+      </Link>
+      <ul>
+        <CustomLink to="/about">About</CustomLink>
+        <CustomLink to="/disclaimer">Disclaimer</CustomLink>
+        <CustomLink to="/download">Download Card Deck</CustomLink>
+        <CustomLink to="/superrr">Superrr Webiste</CustomLink>
+      </ul>
     </nav>
-)
+  );
 }
 
 // /pricing/todos
-function CustomLink({ to, children, ...props}) {
-    const resolvedPath = useResolvedPath(to)
-    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
- return (
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  return (
     <li className={isActive ? "active" : " "}>
-        <Link to={to} {...props}>
-            {children}
-        </Link>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
     </li>
- )
+  );
 }
 
-export default Navbar
+export default Navbar;
