730dc062ba4596b319140ab61b2f9212275b1b56
GrannyYetta <you@example.com>
2022-10-28 17:34:13 +0200

    font conundrum

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from "react-router-dom"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./fonts/Basteleur-Bold.otf";
+import "./fonts/UniversOblique.ttf";
+import "./fonts/Riposte-Light.otf";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-
-
